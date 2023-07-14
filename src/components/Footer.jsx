e5eb108a1f9e5bf36cdcde9b7890cd138393b9da
Matthew Shirtliffe <matt@studio200.io>
2023-07-14 19:36:29 +0100

    add aria labels to social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,36 @@ const Footer = () => {
   return (
     <footer className={footerStyles.footer}>
       <div className={footerStyles.footerContent}>
-        <a href="https://twitter.com/MShirtliffe">
+        <a
+          href="https://twitter.com/MShirtliffe"
+          aria-label="Twitter"
+          target="_blank"
+          rel="noreferrer"
+        >
           <Twitter />
         </a>
-        <a href="https://www.facebook.com/mattshirtliffe">
+        <a
+          href="https://www.facebook.com/mattshirtliffe"
+          aria-label="Facebook"
+          target="_blank"
+          rel="noreferrer"
+        >
           <Facebook />
         </a>
-        <a href="https://www.linkedin.com/in/matthew-shirtliffe-885746246/">
+        <a
+          href="https://www.linkedin.com/in/matthew-shirtliffe-885746246/"
+          aria-label="Linkedin"
+          target="_blank"
+          rel="noreferrer"
+        >
           <Linkedin />
         </a>
-        <a href="https://github.com/mattshirtliffe">
+        <a
+          href="https://github.com/mattshirtliffe"
+          aria-label="GitHub"
+          target="_blank"
+          rel="noreferrer"
+        >
           <GitHub />
         </a>
         <p>Copyright &copy; Matthew Shirtliffe {new Date().getFullYear()}</p>
