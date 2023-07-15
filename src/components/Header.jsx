96d80da94c25a71f0e76107aa205495f7d118166
Matthew Shirtliffe <matt@studio200.io>
2023-07-15 13:05:43 +0100

    added mobile nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as headerStyles from '../styles/header.module.css'
 import { useSiteMetadata } from '../hooks/use-site-metadata'
 
 const Header = () => {
+  const [showHamburger, setShowHamburger] = useState(false)
+
   const { contactEmail } = useSiteMetadata()
   return (
     <header className={headerStyles.navbar}>
@@ -14,6 +16,35 @@ const Header = () => {
           Contact
         </a>
       </nav>
+      <nav
+        className={`${headerStyles.mobileNav} ${
+          showHamburger ? headerStyles.openMobileNav : ''
+        }`}
+      >
+        <a className={headerStyles.contact} href={`mailto:${contactEmail}`}>
+          Contact
+        </a>
+      </nav>
+      <button
+        className={headerStyles.hamburger}
+        onClick={() => setShowHamburger(!showHamburger)}
+      >
+        <div
+          className={`${headerStyles.bar} ${
+            showHamburger ? headerStyles.animateBar1 : ''
+          }`}
+        ></div>
+        <div
+          className={`${headerStyles.bar} ${
+            showHamburger ? headerStyles.animateBar2 : ''
+          }`}
+        ></div>
+        <div
+          className={`${headerStyles.bar} ${
+            showHamburger ? headerStyles.animateBar3 : ''
+          }`}
+        ></div>
+      </button>
     </header>
   )
 }
